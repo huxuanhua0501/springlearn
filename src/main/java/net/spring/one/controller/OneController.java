bd1c104fbeeb05a01cdac7d37d52d92996061153
huxhua163 <huxhua163@163.com>
2016-06-15 17:42:46 +0800

    123

--- src/main/java/net/spring/one/controller/OneController.java
+++ src/main/java/net/spring/one/controller/OneController.java
@@ -1,5 +1,15 @@
 package net.spring.one.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/oneController")
 public class OneController {
 
+	@RequestMapping(value = "/getOne", method = { RequestMethod.GET, RequestMethod.POST })
+	public String getOne() {
+		return null;
+	}
 }
